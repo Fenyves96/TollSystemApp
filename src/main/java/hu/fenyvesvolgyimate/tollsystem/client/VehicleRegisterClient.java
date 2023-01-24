9f3b1d41f849c99dac349652c6c2d42145429645
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2023-01-24 16:29:52 +0100

    introduce VehicleRegisterClient interface

--- src/main/java/hu/fenyvesvolgyimate/tollsystem/client/VehicleRegisterClient.java
+++ src/main/java/hu/fenyvesvolgyimate/tollsystem/client/VehicleRegisterClient.java
@@ -1,46 +1,7 @@
 package hu.fenyvesvolgyimate.tollsystem.client;
 
 import hu.fenyvesvolgyimate.tollsystem.entity.Vehicle;
-import hu.fenyvesvolgyimate.tollsystem.parser.JsonParser;
-import hu.fenyvesvolgyimate.tollsystem.parser.VehicleJsonParser;
-import hu.fenyvesvolgyimate.tollsystem.validaton.VehicleValidator;
-import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleReader;
-import hu.fenyvesvolgyimate.vehicleregisterapp.interactor.VehicleRegister;
-import hu.fenyvesvolgyimate.vehicleregisterapp.presenter.VehiclePresenter;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-public class VehicleRegisterClient implements VehiclePresenter {
-    public void setVehicleReader(VehicleReader vehicleReader) {
-        this.vehicleReader = vehicleReader;
-    }
-
-    VehicleReader vehicleReader;
-    VehicleValidator vehicleValidator = new VehicleValidator();
-    VehicleJsonParser vehicleJsonParser = new VehicleJsonParser();
-
-    Vehicle vehicleResult;
-
-
-    public Vehicle getVehicleByRegistrationNumber(String registrationNumber) {
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("registrationNumber", registrationNumber);
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
-        vehicleReader.getVehicleByRegisterNumber(jsonObject.toString());
-        return vehicleResult;
-    }
-
-    @Override
-    public void displayVehicle(String vehicleJson) {
-        vehicleValidator.validateVehicleJsonResponse(vehicleJson);
-        vehicleResult = vehicleJsonParser.parseJsonToVehicle(vehicleJson);
-    }
-
-    @Override
-    public void displayMessage(String s) {
-        throw new IllegalStateException();
-    }
+public interface VehicleRegisterClient {
+    public Vehicle getVehicleByRegistrationNumber(String registrationNumber);
 }
